9b30101a47bd113a9e30b84b3d6d876e2266fd4e
Christoph Matthies <christoph.matthies@student.hpi.uni-potsdam.de>
2015-05-29 14:13:32 +0200

    run_metrics command takes parameters

--- metricsapp/management/commands/run_metrics.py
+++ metricsapp/management/commands/run_metrics.py
@@ -2,12 +2,22 @@ from django.core.management.base import BaseCommand, CommandError
 from metricsapp.models import Metric, SprintMetric
 
 class Command(BaseCommand):
-    help = 'Runs all available metrics.'
+	help = 'Runs all available metrics.'
 
-    # def add_arguments(self, parser):
-    #     parser.add_argument('poll_id', nargs='+', type=int)
+		#parser.add_argument('poll_id', nargs='+', type=int)
+	def add_arguments(self, parser):
+		parser.add_argument("-m", "--metric-names", type=str, dest="explicit", nargs="+")
 
-    def handle(self, *args, **options):
-        for metric in Metric.objects.all().select_subclasses():
-            self.stdout.write('Running "{}"'.format(metric))
-            metric.run()
+	def handle(self, *args, **options):
+		all_metrics = Metric.objects.select_subclasses()
+		explicit = options['explicit']
+		if explicit:
+			explicit_metrics = [all_metrics.get(name=name) for name in explicit]
+			self._run_metrics(explicit_metrics)
+		else:
+			self._run_metrics(all_metrics)
+
+	def _run_metrics(self, lst):
+		for metric in lst:
+			self.stdout.write('Running "{}"'.format(metric))
+			metric.run()
