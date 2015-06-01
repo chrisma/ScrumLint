60acaea9b093d0f1623d84973012e74257999117
Christoph Matthies <christoph.matthies@student.hpi.uni-potsdam.de>
2015-06-01 15:09:44 +0200

    Added team to daily user story throughput

--- metricsapp/models/daily_user_story_throughput.py
+++ metricsapp/models/daily_user_story_throughput.py
@@ -3,20 +3,14 @@ from ..settings import conf
 import json
 
 class DailyUserStoryThroughput(SprintMetric):
-	def _calculate_score(self, sprint=conf.sprints[-1]):
+	def _calculate_score(self, sprint, team):
 		SCORE_COLUMN = 'USperDev'
 		UPPER_BOUND = 10
 		LOWER_BOUND = 0.2
 		
-		if isinstance(self.results, str):
-			results = json.loads(self.results)
-			print('GOT A STRING, WANTED A DICT')
-		else:
-			results = self.results
-		results = results[sprint]
-		
-		score_index = results['columns'].index(SCORE_COLUMN)
-		value = results['rows'][0][score_index]
+		value = self.get_value(sprint, team, SCORE_COLUMN)
+		if value is None:
+			value = 0
 		
 		if value > UPPER_BOUND or value < LOWER_BOUND:
 			return 25
