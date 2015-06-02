7a63a30a0134a065ac8bdd18aab72e4f547a2d54
Christoph Matthies <christoph.matthies@student.hpi.uni-potsdam.de>
2015-06-02 19:11:05 +0200

    Styling of admin detail page for metrics

--- metricsapp/admin.py
+++ metricsapp/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
 from .models import Metric, Category
+import json
 
-admin.site.register(Metric)
 admin.site.register(Category)
+
+@admin.register(Metric)
+class MetricAdmin(admin.ModelAdmin):
+	save_on_top = True # Add another save button at the top of the form
+	# Columns to be shown on overview page
+	list_display = ('__str__', 'list_of_categories', 'active',)
+	# Model attributes to show filters for (on the right side)
+	list_filter = ('active',)
+	readonly_fields = ('last_query', 'formatted_results',)
+	exclude = ('results',)
+
+	def list_of_categories(self, metric):
+		return ', '.join([c.name for c in metric.categories.all()])
+
+	def formatted_results(self, instance):
+		pretty_json = json.dumps(instance.results, indent=2, sort_keys=True)
+		return '<pre>{}</pre>'.format(pretty_json)
+	formatted_results.short_description = "results"
+	formatted_results.allow_tags = True
