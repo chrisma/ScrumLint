fcd479be6bc6c2dd018bc06014e227540c96e156
Christoph Matthies <christoph.matthies@gmail.com>
2016-03-08 13:34:20 +0100

    Update Neo4J database location and return full users object in 'Personal Code Ownership'.

--- metricsapp/scripts/seed.py
+++ metricsapp/scripts/seed.py
@@ -163,7 +163,7 @@ def import_data():
     metricsapp_dailyuserstorythroughput_1.description = 'Amount of user story a developer is assigned on average per day.'
     metricsapp_dailyuserstorythroughput_1.explanation = 'The average amount of user stories that were assigned to a developer in a sprint (i.e. #sprintUS / #devs). This is without regard to the complexity of each story. A good visualization for this is the amount of user stories a developer has to accomplish on a full work day on average (i.e. (#sprintUS / #devs) / #workDays). If this is significantly lower than average / previously, the stories possibly are too large and should be split, to allow easier parallel work and future estimation. If this number is high (especially higher than a critical threshold [10?]), it is possible that nonfunctional requirements (e.g. tests, design requirements, deployment, “definition of done”) and communication and context  switching overhead were underrated and there are too many stories in the sprint.'
     metricsapp_dailyuserstorythroughput_1.query = "MATCH (l:GithubLabel)-[:labels]-(i:GithubIssue)-[:milestone]->(m:GithubMilestone) WHERE m.title = '{sprint}' AND l.name = '{label}' WITH m.title as Sprint, count(i) as AmountOfIssues, l.name as Team MATCH (u:GithubUser) WHERE u.role <> 'org' AND u.team = '{team}' RETURN AmountOfIssues, count(u) as devs, round(100 * (toFloat(AmountOfIssues) / count(u))) / 100 as USperDev"
-    metricsapp_dailyuserstorythroughput_1.endpoint = 'http://192.168.30.196:7478/db/data/transaction/commit'
+    metricsapp_dailyuserstorythroughput_1.endpoint = 'http://192.168.30.233:7478/db/data/transaction/commit'
     metricsapp_dailyuserstorythroughput_1.active = True
     metricsapp_dailyuserstorythroughput_1.severity = 0.5
     metricsapp_dailyuserstorythroughput_1 = importer.save_or_locate(metricsapp_dailyuserstorythroughput_1)
@@ -179,7 +179,7 @@ def import_data():
     metricsapp_justintimedevelopment_1.description = 'Commits within 30 minutes sprint deadline.'
     metricsapp_justintimedevelopment_1.explanation = 'Amount of commits during the last day / last week of the sprint. If everything was last minute, the Scrum meetings were ineffective, due to lack of content. Furthermore, blockers for / from other teams could not be communicated.\r\n'
     metricsapp_justintimedevelopment_1.query = 'MATCH (m:GithubMilestone), (g:GithubCommit)-[:committer]-(u:GithubUser) WHERE u.team = "{team}" WITH m, g, (m.due_on+60*60*15) - g.commit_committer_date as SecsDistance WHERE SecsDistance>0 AND SecsDistance<60*60*72 AND m.title = "{sprint}" WITH m, count(g) as JITcommits MATCH (h:GithubCommit)-[:committer]-(u:GithubUser) WHERE h.commit_committer_date < m.due_on AND u.team = "{team}" WITH JITcommits, count(h) as TotalCommits RETURN JITcommits, TotalCommits, (JITcommits*1.0)/TotalCommits as Percentage'
-    metricsapp_justintimedevelopment_1.endpoint = 'http://192.168.30.196:7478/db/data/transaction/commit'
+    metricsapp_justintimedevelopment_1.endpoint = 'http://192.168.30.233:7478/db/data/transaction/commit'
     metricsapp_justintimedevelopment_1.active = True
     metricsapp_justintimedevelopment_1.severity = 1.0
     metricsapp_justintimedevelopment_1 = importer.save_or_locate(metricsapp_justintimedevelopment_1)
@@ -194,7 +194,7 @@ def import_data():
     metricsapp_forgottenones_1.description = 'User stories that are still open in previous sprints.'
     metricsapp_forgottenones_1.explanation = "Amount of user stories in previous sprints that are not closed. It is likely that these user stories were worked on, but were simply not closed due to negligence or were “backup” stories that  weren't of high priority. These stories should be closed (or moved) to promote better overview or need special attention if they really were forgotten."
     metricsapp_forgottenones_1.query = 'MATCH (m:GithubMilestone)-[:milestone]-(i:GithubIssue)-[:labels]-(l:GithubLabel) WHERE m.title = "{sprint}" and m.due_on<timestamp() and i.state="open" and l.name = "{label}" WITH m, m.title as Sprint, collect(DISTINCT i) as OpenIssues, count(DISTINCT i) as Amount MATCH m-[:milestone]-(j:GithubIssue)-[:labels]-(l:GithubLabel) WHERE l.name = "{label}" WITH OpenIssues, count(j) as Total, Amount RETURN Amount, OpenIssues, Total, (Amount*1.0)/Total as Percentage'
-    metricsapp_forgottenones_1.endpoint = 'http://192.168.30.196:7478/db/data/transaction/commit'
+    metricsapp_forgottenones_1.endpoint = 'http://192.168.30.233:7478/db/data/transaction/commit'
     metricsapp_forgottenones_1.active = True
     metricsapp_forgottenones_1.severity = 0.5
     metricsapp_forgottenones_1 = importer.save_or_locate(metricsapp_forgottenones_1)
@@ -209,7 +209,7 @@ def import_data():
     metricsapp_neverendingstory_1.description = 'User stories that were in the backlog of 3 or more sprints.'
     metricsapp_neverendingstory_1.explanation = 'A user story that is assigned to the sprint backlog of more than 3 sprints, with or without commits referencing it. The more never-ending stories there are in a project, the more likely it is that blockers and dependencies will become an issue.\r\n'
     metricsapp_neverendingstory_1.query = 'Match (e:GithubIssueEvent)-[:issue]-(i:GithubIssue)-[:labels]-(l:GithubLabel) WHERE e.event="milestoned" AND e.milestone_title IN [{sprint_list}] AND l.name = "{label}" WITH i, collect(DISTINCT e.milestone_title) as Sprints WITH i, Sprints, length(Sprints) as InSprints WHERE InSprints > 2 RETURN i as Issues, InSprints, Sprints'
-    metricsapp_neverendingstory_1.endpoint = 'http://192.168.30.196:7478/db/data/transaction/commit'
+    metricsapp_neverendingstory_1.endpoint = 'http://192.168.30.233:7478/db/data/transaction/commit'
     metricsapp_neverendingstory_1.active = True
     metricsapp_neverendingstory_1.severity = 1.0
     metricsapp_neverendingstory_1 = importer.save_or_locate(metricsapp_neverendingstory_1)
@@ -224,7 +224,7 @@ def import_data():
     metricsapp_commitsperdev_1.description = 'Amount of commits per Developer.'
     metricsapp_commitsperdev_1.explanation = '"Commit early, commit often" is an important mantra when developing in large teams. It allows coworkers to build on functionality and makes version control easier.\r\n'
     metricsapp_commitsperdev_1.query = 'MATCH (m:GithubMilestone)-[:milestone]-(c:GithubCommit)-[:author]-(u:GithubUser) WHERE u.team = "{team}" and m.title = "{sprint}" and u.role <> "org" and c.merge = FALSE WITH count(c) AS Commits, avg(c.total) AS AverageChangedLines MATCH (u:GithubUser) WHERE u.role <> "org" AND u.team = "{team}" RETURN Commits, count(u) as devs, AverageChangedLines, round(100 * (toFloat(Commits) / count(u))) / 100 as CommitsperDev'
-    metricsapp_commitsperdev_1.endpoint = 'http://192.168.30.196:7478/db/data/transaction/commit'
+    metricsapp_commitsperdev_1.endpoint = 'http://192.168.30.233:7478/db/data/transaction/commit'
     metricsapp_commitsperdev_1.active = True
     metricsapp_commitsperdev_1.severity = 0.5
     metricsapp_commitsperdev_1 = importer.save_or_locate(metricsapp_commitsperdev_1)
@@ -239,7 +239,7 @@ def import_data():
     metricsapp_speedyprs_1.description = 'Pull requests that were closed within 30 minutes without comments.'
     metricsapp_speedyprs_1.explanation = 'Pull requests can be a tool to help inform team members what functionality is added in a collection of commits. It allows team members and stakeholders to comment and perform code review. Furthermore, continuous integration services can run the proposed changes, making sure all tests pass. If pull requests are closed in a short timespan, many of these possibilities remain unused.\r\n'
     metricsapp_speedyprs_1.query = 'MATCH (m:GithubMilestone)-[:milestone]-(pri:PullRequestIssue)-[:user]-(u:GithubUser) WHERE pri.state="closed" AND pri.comments=0 AND u.team="{team}" AND m.title = "{sprint}" WITH pri, (pri.closed_at-pri.created_at) as ClosedWithinSecs WHERE ClosedWithinSecs < (60*60*0.5) WITH count(pri) as Amount, collect(pri) as PullRequests, collect(ClosedWithinSecs) as ClosingTimes MATCH (m:GithubMilestone)-[:milestone]-(all:PullRequestIssue)-[:user]-(u:GithubUser) WHERE u.team="{team}" AND m.title = "{sprint}" WITH PullRequests, count(all) as Total, extract(x IN ClosingTimes | round(x/60.0)) as ClosedWithinMinutes, Amount RETURN PullRequests, ClosedWithinMinutes, Amount, Total, Amount/(Total*1.0) as Percentage'
-    metricsapp_speedyprs_1.endpoint = 'http://192.168.30.196:7478/db/data/transaction/commit'
+    metricsapp_speedyprs_1.endpoint = 'http://192.168.30.233:7478/db/data/transaction/commit'
     metricsapp_speedyprs_1.active = True
     metricsapp_speedyprs_1.severity = 0.5
     metricsapp_speedyprs_1 = importer.save_or_locate(metricsapp_speedyprs_1)
@@ -253,8 +253,8 @@ def import_data():
     metricsapp_personal_code_ownership_1.name = "Personal Code Ownership"
     metricsapp_personal_code_ownership_1.description = 'Code is edited heavily by few developers.'
     metricsapp_personal_code_ownership_1.explanation = 'Collective Code Ownership, the convention, that every team member is not only allowed, but in fact has a positive duty, to make changes to any code file as necessary, is part of agile development (especially XP). It can help reduce the risk that the absence of a few developer will stall work on a certain area of code. Furthermore it can help prevent "Conway\'s Law", helps share technical knowledge between developers and encourages each developer to feel responsible for the quality of the whole.\r\n'
-    metricsapp_personal_code_ownership_1.query = 'MATCH (fc:GithubFileChange)-[:files]-(c:NonMergeCommit)-[]-(u:GithubUser) WHERE has(fc.filename) AND fc.filename<>"" AND u.role <> "org" AND u.team = "{team}" MATCH c-[:milestone]-(m:GithubMilestone) WHERE m.title="{sprint}" WITH fc, u ORDER BY u.login WITH fc.filename as Filename, collect(DISTINCT u.login) as Authors, count(fc) as Edits WITH Filename, Authors, Edits, length(Authors) as AuthorAmount WHERE Edits > 8 AND AuthorAmount < 3 RETURN Filename,  AuthorAmount, Authors, Edits ORDER BY AuthorAmount ASC, Edits DESC'
-    metricsapp_personal_code_ownership_1.endpoint = 'http://192.168.30.196:7478/db/data/transaction/commit'
+    metricsapp_personal_code_ownership_1.query = 'MATCH (fc:GithubFileChange)-[:files]-(c:NonMergeCommit)-[]-(u:GithubUser) WHERE has(fc.filename) AND fc.filename<>"" AND u.role <> "org" AND u.team = "{team}" MATCH c-[:milestone]-(m:GithubMilestone) WHERE m.title="{sprint}" WITH fc, u ORDER BY u.login WITH fc.filename as Filename, collect(DISTINCT u) as Authors, count(fc) as Edits WITH Filename, Authors, Edits, length(Authors) as AuthorAmount WHERE Edits > 8 AND AuthorAmount < 3 RETURN Filename,  AuthorAmount, Authors, Edits ORDER BY AuthorAmount ASC, Edits DESC'
+    metricsapp_personal_code_ownership_1.endpoint = 'http://192.168.30.233:7478/db/data/transaction/commit'
     metricsapp_personal_code_ownership_1.active = True
     metricsapp_personal_code_ownership_1.severity = 1.0
     metricsapp_personal_code_ownership_1 = importer.save_or_locate(metricsapp_personal_code_ownership_1)
@@ -269,7 +269,7 @@ def import_data():
     metricsapp_monster_stories_1.description = 'User stories that are too large.'
     metricsapp_monster_stories_1.explanation = 'User stories should be small enough to get a quick overview of the work to be done, but should contain enough information to be estimatable by the developers. It is sometimes stated that user stories should fit on an index card. The issues below have double the average length of issues in the selected sprint or have double the amount of checkboxes of other stories. Both of these can be an indicator that a user story is too large and could be split. Smaller user stories can improve the ability to plan as they are less likely to deviate from the estimation and sprint backlogs can be filled on a more granular basis.\r\n'
     metricsapp_monster_stories_1.query = 'MATCH (u:GithubUser)-[:user]-(i:GithubIssue)-[:milestone]-(m:GithubMilestone) WHERE i.body <> "" and m.title = "{sprint}" WITH AVG(length(i.body)) as AvgLength MATCH (v:GithubUser)-[:user]-(j:GithubIssue)-[:milestone]-(n:GithubMilestone) WHERE (length(split(j.body,"[ ]"))-1) + (length(split(j.body,"[x]"))-1) <> 0 and n.title = "{sprint}" WITH AVG((length(split(j.body,"[ ]"))-1) + (length(split(j.body,"[x]"))-1)) as AvgAmountCB, AvgLength MATCH (w:GithubUser)-[:user]-(k:GithubIssue)-[:milestone]-(o:GithubMilestone) WHERE w.team = "{team}" and o.title = "{sprint}" AND (length(k.body) >= AvgLength*2 OR (length(split(k.body,"[ ]"))-1) + (length(split(k.body,"[x]"))-1) > AvgAmountCB*2) RETURN count(k) as IssueCount, collect(k) as Issue, collect(length(k.body)) as Length, AvgLength, collect((length(split(k.body,"[ ]"))-1) + (length(split(k.body,"[x]"))-1)) as AmountCheckBoxes, AvgAmountCB'
-    metricsapp_monster_stories_1.endpoint = 'http://192.168.30.196:7478/db/data/transaction/commit'
+    metricsapp_monster_stories_1.endpoint = 'http://192.168.30.233:7478/db/data/transaction/commit'
     metricsapp_monster_stories_1.active = True
     metricsapp_monster_stories_1.severity = 1.0
     metricsapp_monster_stories_1 = importer.save_or_locate(metricsapp_monster_stories_1)
@@ -284,7 +284,7 @@ def import_data():
     metricsapp_untested_complexity_1.description = 'Commits that increased code complexity without increasing code coverage.'
     metricsapp_untested_complexity_1.explanation = 'Having a high percentage of code covered by tests is essential for ensuring the system is thoroughly tested. When committing changes that significantly increase the complexity of the code base (>0.5), tests should be committed as well, increasing the code coverage. If this is not the case for a high percentage of commits, this might be a sign that writing tests and code coverage metrics should be paid more attention to.\r\n'
     metricsapp_untested_complexity_1.query = 'MATCH (u:GithubUser {{team:"{team}"}})-[:committer]-(c:NonMergeCommit)-[:parents]->(parent:NonMergeCommit) WHERE has(c.total_complexity) AND has(c.test_coverage) AND has(parent.total_complexity) AND has(parent.test_coverage) MATCH c-[:milestone]-(m:GithubMilestone {{title:"{sprint}"}}) WITH parent.total_complexity-c.total_complexity as ComplexityChange, parent.test_coverage-c.test_coverage as CoverageChange, c WHERE ComplexityChange > 0.5  AND CoverageChange <= 0 WITH collect(c) as Commits, collect(ComplexityChange) as ComplexityIncrease, collect(CoverageChange) as CoverageChange, count(c) as Amount MATCH (v:GithubUser {{team:"{team}"}})-[:committer]-(d:NonMergeCommit)-[:milestone]-(m:GithubMilestone {{title:"{sprint}"}}) RETURN Commits, ComplexityIncrease, CoverageChange, Amount, (Amount*1.0)/count(d) as Percentage'
-    metricsapp_untested_complexity_1.endpoint = 'http://192.168.30.196:7478/db/data/transaction/commit'
+    metricsapp_untested_complexity_1.endpoint = 'http://192.168.30.233:7478/db/data/transaction/commit'
     metricsapp_untested_complexity_1.active = True
     metricsapp_untested_complexity_1.severity = 0.5
     metricsapp_untested_complexity_1 = importer.save_or_locate(metricsapp_untested_complexity_1)
@@ -299,7 +299,7 @@ def import_data():
     metricsapp_tutor_scores_1.description = 'tutors evaluating sprint meetings.'
     metricsapp_tutor_scores_1.explanation = 'Tutors rate meetings on two levels: dedication and progress. The former signifies how much effort was put into implementing, customizing and trying out new Scrum practices, the latter how well the team works and has internalized Scrum values.\r\n'
     metricsapp_tutor_scores_1.query = 'MATCH (m:GithubMilestone)<-[:milestone]-(n:Meeting) WHERE m.title = "{sprint}" AND n.team = "{team}" WITH n, (n.dedication+n.progress)/2.0 as AveragedRating RETURN n.name as Meetings, n.dedication as Dedication, n.progress as Progress, AveragedRating ORDER BY AveragedRating ASC'
-    metricsapp_tutor_scores_1.endpoint = 'http://192.168.30.196:7478/db/data/transaction/commit'
+    metricsapp_tutor_scores_1.endpoint = 'http://192.168.30.233:7478/db/data/transaction/commit'
     metricsapp_tutor_scores_1.active = True
     metricsapp_tutor_scores_1.severity = 1.0
     metricsapp_tutor_scores_1 = importer.save_or_locate(metricsapp_tutor_scores_1)
@@ -314,7 +314,7 @@ def import_data():
     metricsapp_percent_metric_1.description = 'Issues which are suspected duplicates.'
     metricsapp_percent_metric_1.explanation = 'Issues with comments that point to them being duplicates. User stories that duplicate features of previous user stories can lead to features being implemented multiple times by different teams.\r\n'
     metricsapp_percent_metric_1.query = 'MATCH (l:GithubLabel)-[:labels]-(i:GithubIssue)-[:milestone]->(m:GithubMilestone), (ic:GithubIssueComment) WHERE ic.issue_url=replace("https://api.github.com/repos/hpi-swt2/event-und-raumplanung/issues/{{number}}", "{{number}}", str(i.number)) AND ic.body=~"(?i).*duplicate.*" AND l.name="{label}" AND m.title="{sprint}" WITH count(DISTINCT i) as AmountOfMarkedDuplicates, collect(i) as Issues,l MATCH (o:GithubLabel)-[:labels]-(j:GithubIssue)-[:milestone]->(n:GithubMilestone) WHERE o.name="{label}" AND n.title="{sprint}" RETURN l.name as Team, AmountOfMarkedDuplicates, Issues, count(j) as Total, (AmountOfMarkedDuplicates*1.0)/count(j) as Percentage'
-    metricsapp_percent_metric_1.endpoint = 'http://192.168.30.196:7478/db/data/transaction/commit'
+    metricsapp_percent_metric_1.endpoint = 'http://192.168.30.233:7478/db/data/transaction/commit'
     metricsapp_percent_metric_1.active = True
     metricsapp_percent_metric_1.severity = 0
     metricsapp_percent_metric_1 = importer.save_or_locate(metricsapp_percent_metric_1)
